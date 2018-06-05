4ae973cd8ce4797352fea0cd61651a39208117f4
Nagarajan <x0257679@ti.com>
2018-06-05 18:30:32 +0530

    Added timer

--- popup.js
+++ popup.js
@@ -2,13 +2,46 @@
 var angularApp = angular.module('angularApp', []);
 
 //Controllers
-angularApp.controller('mainController', ['$scope' ,'$http', function($scope, $http){
+angularApp.controller('mainController', ['$scope' ,'$http', '$timeout', function($scope, $http, $timeout){
 
     var APIURL = "https://us-central1-quoteme-8c1dd.cloudfunctions.net/giveMeQuote"
     // var APIURL = 'http://localhost:8080'
 
     $scope.txt = 'Marvel Cinematic Universe-Loading';
-    $scope.author = ''
+    $scope.author = '';
+
+    var endTime = 1528237800000;
+    // var endTime = 0;
+    $scope.hours = '00';
+    $scope.mins = '00';
+    $scope.secs = '00';
+    $scope.timerOn = true;
+
+    $scope.teamSelected = function(teamIndex) {
+        // var team = $scope.teams[teamIndex];
+    };
+
+    $scope.countdown = function() {
+        var time = (new Date()).getTime();
+        if(time < endTime) {
+            var milliseconds = endTime - time;
+            var timeout = milliseconds % 1000;
+            var seconds = parseInt(milliseconds / 1000);
+            $scope.secs = (seconds % 60).toString();
+            $scope.secs = ($scope.secs.length === 1 ?  '0' : '') + $scope.secs;
+            var mins = parseInt(seconds / 60);
+            $scope.mins = (mins % 60).toString();
+            $scope.mins = ($scope.mins.length === 1 ?  '0' : '') + $scope.mins;
+            $scope.hours = (parseInt(mins / 60)).toString();
+            $scope.hours = ($scope.hours.length === 1 ?  '0' : '') + $scope.hours;
+            console.log(1528237800000, time, milliseconds, timeout, seconds, $scope.secs, mins, $scope.mins, $scope.hours);
+            $timeout($scope.countdown, timeout);
+        } else {
+            $scope.timerOn = false;
+        }
+    };
+
+    $scope.countdown();
 
     // $http.get(APIURL, function(response){
     //     console.log(response);
